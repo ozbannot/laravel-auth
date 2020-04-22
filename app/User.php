dfe9d75cd0d691bb32d2708ee67f66f0d414ac53
vagrant <vagrant@localhost.localdomain>
2020-04-22 01:57:21 +0000

    auth table change

--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+    * 使用テーブル名
+    * @var string
+    */
+    protected $table = 'operation_info';
 }
